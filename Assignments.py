73dd446b3728a1b2c79550e824ede2f557be2263
Joona Rantala <joonajani.rantala@gmail.com>
2022-08-25 14:49:04 +0300

    :)

---
+++ Assignments.py
@@ -0,0 +1,22 @@
+#phase 1
+import math
+import random
+
+#character_name = input("What is your name? ")
+
+#print("Hello, " + character_name + "!")
+
+#phase 2
+
+#radius = float(input("Give the radius? "))
+#print(f"Area is {math.pi * radius ** 2:.2f}")
+
+#phase 6
+
+print(f"{random.randint(0, 999):03d}")
+print(str(random.randint(0,9)) + str(random.randint(0, 9)) + str(random.randint(0, 9)))
+
+
+
+
+
