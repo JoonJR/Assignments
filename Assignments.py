4d8d351b415a61045c3f82224974abb7c2739b1c
Joona Rantala <joonajani.rantala@gmail.com>
2022-08-25 15:50:41 +0300

    :)

--- Assignments.py
+++ Assignments.py
@@ -13,6 +13,20 @@ import random
 
 #phase 3
 
+#length = float(input("Give the length of a rectangle? "))
+#width = float(input("Give the width of a rectangle? "))
+#print(f"Perimeter of a rectangle is {length * 2 + width * 2: 1f}")
+#print(f"Area of the rectangle is {length * width :.1f}")
+
+#phase 4
+
+num1 = float(input("Give a number"))
+num2 = float(input("Give a 2nd number"))
+num3 = float(input("Give a 3rd number"))
+print(f"The sum of the 3 numbers is {num1 + num2 + num3:.0f}")
+print(f"The product of numbers is {num1 * num2 * num3:.0f}")
+print(f"The average of your numbers is {(num1 + num2 + num3)/3:.2f}")
+
 #phase 6
 
 #print(f"{random.randint(0, 999):03d}")
