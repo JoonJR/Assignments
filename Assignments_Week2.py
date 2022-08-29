50419b010082a5d8bb08b6243ed3921d49daa43d
Joona Rantala <joonajani.rantala@gmail.com>
2022-08-29 19:10:23 +0300

    :)

--- Assignments_Week2.py
+++ Assignments_Week2.py
@@ -1,29 +1,69 @@
-s_code1 = "NATO"
-s_code2 = "EU"
+#exercise 1
 
-answer = input("What is the secret code? ")
-if answer == s_code1:
-    print("Correct")
-elif answer == s_code2:
-    print("Also correct")
-    s_number = int(input("What is the secret number? "))
-    if s_number % 4 == 0:
-        print("You are lucky!")
-    else:
-        print("You are not lucky.")
-elif answer == "TOPSECRET":
-    print("You got it!!")
+fish_length = int(input("Enter length of the fish you caught in centimeters: "))
+limit = 42
+length_missing = limit - fish_length
+if fish_length < limit:
+    print("Release fish back to the lake, it needs to grow " + str(length_missing) + " centimeters." )
 else:
-    print("Incorrect")
+    print("The fish is over 42cm and it meets the size limit.")
 
+#exercise 2
 
+cabin_class = input("Enter the cabin class (LUX, A, B or C): ")
+class_lux = "LUX"
+class_a = "A"
+class_b = "B"
+class_c = "C"
 
+if cabin_class == class_lux:
+    print("LUX: upper-deck cabin with a balcony.")
+elif cabin_class == class_a:
+    print("A: above the car deck, equipped with a window.")
+elif cabin_class == class_b:
+    print("B: windowless cabin above the car deck.")
+elif cabin_class == class_c:
+    print("C: windowless cabin below the car deck.")
+else:
+    print("Invalid cabin class.")
 
-#phase 1
+#exercise 3
 
-#zander = float(input("Enter the length of a zander you just caught: "))
+user_male = "M" or "m"
+user_female = "F"
+normal_hv = "Your hemoglobin value is normal."
+low_hv = "Your hemoglobin value is lower than normal."
+high_hv = "Your hemoglobin value is higher than normal."
 
-#if zander >= 42:
-#    print("Good job, you can eat today.")
-#else:
-#   print("Unfortunately the zander is ?? cm under size limit, you'll have to release it. ")
+user_gender = input("What is your biological gender? (M or F): ")
+user_hv = float(input("What is your hemoglobin value? (g/l): "))
+if user_gender == user_male:
+    if user_hv >= 137 and user_hv <= 167:
+        print(normal_hv)
+    elif user_hv < 137:
+        print(low_hv)
+    elif user_hv > 167:
+        print(high_hv)
+elif user_gender == user_female:
+    if user_hv >= 117 and user_hv <= 155:
+        print(normal_hv)
+    elif user_hv < 117:
+        print(low_hv)
+    elif user_hv > 155:
+        print(high_hv)
+else:
+    print("Input error. Use capital letters")
+
+#exercise 4
+
+year = int(input("Enter a year to see if it's a leap year: "))
+division_modulo_4 = year % 4
+division_modulo_100 = year % 100
+division_modulo_400 = year % 400
+if division_modulo_4 == 0:
+    if division_modulo_100 == 0 and division_modulo_400 !=0:
+        print("This year is not a leap year.")
+    else:
+        print("This year is a leap year.")
+else:
+    print("This year is not a leap year.")
---
+++ Assignments_Week3.py
@@ -0,0 +1,17 @@
+#s_code1 = "NATO"
+#s_code2 = "EU"
+
+#answer = input("What is the secret code? ")
+#if answer == s_code1:
+#    print("Correct")
+#elif answer == s_code2:
+#    print("Also correct")
+#    s_number = int(input("What is the secret number? "))
+#    if s_number % 4 == 0:
+#        print("You are lucky!")
+#    else:
+#        print("You are not lucky.")
+#elif answer == "TOPSECRET":
+#    print("You got it!!")
+#else:
+#    print("Incorrect")
