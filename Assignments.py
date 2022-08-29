cd1dbb6cebb6333dccc3ab6cb0a3ce99616f234c
Joona Rantala <joonajani.rantala@gmail.com>
2022-08-29 12:39:54 +0300

    :)

--- Assignments.py
+++ Assignments.py
@@ -2,30 +2,30 @@
 import math
 import random
 
-#character_name = input("What is your name? ")
+character_name = input("What is your name? ")
 
-#print("Hello, " + character_name + "!")
+print("Hello, " + character_name + "!")
 
 #phase 2
 
-#radius = float(input("Give the radius? "))
-#print(f"Area is {math.pi * radius ** 2:.2f}")
+radius = float(input("Give the radius? "))
+print(f"Area is {math.pi * radius ** 2:.2f}")
 
 #phase 3
 
-#length = float(input("Give the length of a rectangle? "))
-#width = float(input("Give the width of a rectangle? "))
-#print(f"Perimeter of a rectangle is {length * 2 + width * 2: 1f}")
-#print(f"Area of the rectangle is {length * width :.1f}")
+length = float(input("Give the length of a rectangle: "))
+width = float(input("Give the width of a rectangle: "))
+print(f"Perimeter of a rectangle is {length * 2 + width * 2: 1f}")
+print(f"Area of the rectangle is {length * width :.1f}")
 
 #phase 4
 
-#num1 = float(input("Give a number"))
-#num2 = float(input("Give a 2nd number"))
-#num3 = float(input("Give a 3rd number"))
-#print(f"The sum of the 3 numbers is {num1 + num2 + num3:.0f}")
-#print(f"The product of numbers is {num1 * num2 * num3:.0f}")
-#print(f"The average of your numbers is {(num1 + num2 + num3)/3:.2f}")
+num1 = float(input("Give a number: "))
+num2 = float(input("Give a 2nd number: "))
+num3 = float(input("Give a 3rd number: "))
+print(f"The sum of the 3 numbers is {num1 + num2 + num3:.0f}")
+print(f"The product of numbers is {num1 * num2 * num3:.0f}")
+print(f"The average of your numbers is {(num1 + num2 + num3)/3:.2f}")
 
 #phase 5
 
@@ -42,8 +42,8 @@ print(f"The weight in modern units is: {weight_kilos} kg and {weight_gramms_left
 
 #phase 6
 
-#print(f"{random.randint(0, 999):03d}")
-#print(str(random.randint(0,9)) + str(random.randint(0, 9)) + str(random.randint(0, 9)))
+print(f"{random.randint(0, 999):03d}")
+print(str(random.randint(0,9)) + str(random.randint(0, 9)) + str(random.randint(0, 9)))
 
 
 
