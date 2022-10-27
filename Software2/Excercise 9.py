e5ebb4cdf1e14d0b57cb4b6e7f62cd884c15e60f
JoonJR <joonajani.rantala@gmail.com>
2022-10-27 10:47:58 +0300

    done

--- Software2/Excercise 9.py
+++ Software2/Excercise 9.py
@@ -1,5 +1,10 @@
+import random
+from prettytable import PrettyTable
+
 # exercise 1
 print("\n************************Exercise 1*****************************\n")
+
+
 class Car:
     def __init__(self, reg, max_speed, curr_speed=0, distance_travelled=0):
         self.reg = reg
@@ -11,19 +16,19 @@ class Car:
 car1 = Car("ABC-123", 142)
 
 print('First cars registration plate is', car1.reg, 'and the maximum speed on the car is', car1.max_speed, '.'
-                                                                                                           'The current speed is',
-      car1.curr_speed, 'and distance travelled is', car1.distance_travelled)
+      'The current speed is', car1.curr_speed, 'and distance travelled is', car1.distance_travelled)
 
 
 # exercise 2
 print("\n************************Exercise 2*****************************\n")
+
+
 class Car:
     def __init__(self, reg, max_speed, curr_speed=0, distance_travelled=0):
         self.reg = reg
         self.max_speed = max_speed
         self.curr_speed = curr_speed
         self.distance_travelled = distance_travelled
-
 
     def accelerate(self, change_of_speed):
         if self.max_speed >= self.curr_speed + change_of_speed >= 0:
@@ -37,23 +42,24 @@ class Car:
 
 car1 = Car("ABC-123", 142)
 
-print('Now the car accelerates to 30 km/h, new current speed of the car is', car1.accelerate(30),'km/h')
-print('Car enters highway and accelerates to correct speed', car1.accelerate(70),'km/h')
-print('New sports car, ofcourse you need to try how the power feels, drivers press gas pedal to the floor and speeds to'
-      ,car1.accelerate(50),'km/h')
+print('Now the car accelerates to 30 km/h, new current speed of the car is', car1.accelerate(30), 'km/h')
+print('Car enters highway and accelerates to correct speed', car1.accelerate(70), 'km/h')
+print('New sports car, ofcourse you need to try how the power feels, drivers press gas pedal to the floor and speeds '
+      'to', car1.accelerate(50), 'km/h')
 print('It only goes 142km/h?? Driver spots a cop car and has to brake hard, looks like the brakes are too good'
       ', car is now going at', car1.accelerate(-200))
 
 
 # exercise 3
 print("\n************************Exercise 3*****************************\n")
+
+
 class Car:
     def __init__(self, reg, max_speed, curr_speed=0, distance_travelled=2000):
         self.reg = reg
         self.max_speed = max_speed
         self.curr_speed = curr_speed
         self.distance_travelled = distance_travelled
-
 
     def accelerate(self, change_of_speed):
         if self.max_speed >= self.curr_speed + change_of_speed >= 0:
@@ -78,8 +84,6 @@ print(f"The distance traveled in 1.5h at the constant speed of 60 km/h is {car.d
 
 # Exercise 4
 print("\n************************Exercise 4*****************************\n")
-import random
-from prettytable import PrettyTable
 
 
 class Car:
@@ -103,17 +107,17 @@ class Car:
 
 
 car_list = []
-for i in range(1,10):
+for i in range(1, 10):
     new_car = Car("ABC-" + str(i), random.randint(100, 200))
     car_list.append(new_car)
 
 race_on = True
 
 while race_on:
-    for car in range (len(car_list)):
+    for car in range(len(car_list)):
         if car_list[car].distance_travelled <= 10000:
             car_list[car].drive(1)
-            car_list[car].accelerate(random.randint(-10,15))
+            car_list[car].accelerate(random.randint(-10, 15))
         else:
             print(f"The car {car_list[car].reg} won the race!!")
             race_on = False
@@ -123,7 +127,3 @@ my_table = PrettyTable(["Registration number", "Distance traveled", "Max speed:"
 for car in range(len(car_list)):
     my_table.add_row([car_list[car].reg, car_list[car].distance_travelled, car_list[car].max_speed])
 print(my_table)
-
-
-
-
