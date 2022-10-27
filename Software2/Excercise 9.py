a8820fc01f4c8038d6228682c99f7fb871c19fd6
JoonJR <joonajani.rantala@gmail.com>
2022-10-27 10:32:51 +0300

    done

--- Software2/Excercise 9.py
+++ Software2/Excercise 9.py
@@ -1,5 +1,5 @@
 # exercise 1
-"""""
+print("\n************************Exercise 1*****************************\n")
 class Car:
     def __init__(self, reg, max_speed, curr_speed=0, distance_travelled=0):
         self.reg = reg
@@ -16,7 +16,7 @@ print('First cars registration plate is', car1.reg, 'and the maximum speed on th
 
 
 # exercise 2
-
+print("\n************************Exercise 2*****************************\n")
 class Car:
     def __init__(self, reg, max_speed, curr_speed=0, distance_travelled=0):
         self.reg = reg
@@ -46,9 +46,9 @@ print('It only goes 142km/h?? Driver spots a cop car and has to brake hard, look
 
 
 # exercise 3
-
+print("\n************************Exercise 3*****************************\n")
 class Car:
-    def __init__(self, reg, max_speed, curr_speed=0, distance_travelled=0):
+    def __init__(self, reg, max_speed, curr_speed=0, distance_travelled=2000):
         self.reg = reg
         self.max_speed = max_speed
         self.curr_speed = curr_speed
@@ -65,14 +65,65 @@ class Car:
         return self.curr_speed
 
     def drive(self, number_of_hours):
-
         self.distance_travelled += number_of_hours * self.curr_speed
 
 
 car = Car("ABC-123", 142)
 car.accelerate(60)
 car.traveled_distance = 2000
-print(f"The initial distance is {car.distance_travelled} km/h")
+print(f"The initial distance is {car.distance_travelled} km")
 car.drive(1.5)
 print(f"The distance traveled in 1.5h at the constant speed of 60 km/h is {car.distance_travelled} km")
-"""""
+
+
+# Exercise 4
+print("\n************************Exercise 4*****************************\n")
+import random
+from prettytable import PrettyTable
+
+
+class Car:
+    def __init__(self, reg, max_speed, curr_speed=0, distance_travelled=0):
+        self.reg = reg
+        self.max_speed = max_speed
+        self.curr_speed = curr_speed
+        self.distance_travelled = distance_travelled
+
+    def accelerate(self, change_of_speed):
+        if self.max_speed >= self.curr_speed + change_of_speed >= 0:
+            self.curr_speed = self.curr_speed + change_of_speed
+        elif self.max_speed < self.curr_speed + change_of_speed:
+            self.curr_speed = self.max_speed
+        elif self.curr_speed + change_of_speed < 0:
+            self.curr_speed = 0
+        return self.curr_speed
+
+    def drive(self, number_of_hours):
+        self.distance_travelled += number_of_hours * self.curr_speed
+
+
+car_list = []
+for i in range(1,10):
+    new_car = Car("ABC-" + str(i), random.randint(100, 200))
+    car_list.append(new_car)
+
+race_on = True
+
+while race_on:
+    for car in range (len(car_list)):
+        if car_list[car].distance_travelled <= 10000:
+            car_list[car].drive(1)
+            car_list[car].accelerate(random.randint(-10,15))
+        else:
+            print(f"The car {car_list[car].reg} won the race!!")
+            race_on = False
+
+
+my_table = PrettyTable(["Registration number", "Distance traveled", "Max speed:"])
+for car in range(len(car_list)):
+    my_table.add_row([car_list[car].reg, car_list[car].distance_travelled, car_list[car].max_speed])
+print(my_table)
+
+
+
+
