eeef2aeaac8b83dfdec1b4e681b8b578e05191a5
JoonJR <joonajani.rantala@gmail.com>
2022-11-02 23:51:41 +0200

    .

--- Software2/Exercise 10 .py
+++ Software2/Exercise 10 .py
@@ -1,15 +1,41 @@
+import random
+
 # exercise 1
 print("\n************************Exercise 1*****************************\n")
 
 class Elevator:
 
-    def __init__(self, ):
-    def go_to_floor:
+    def __init__(self, bottom_floor, top_floor):
+        self.bottom_floor = bottom_floor
+        self.top_floor = top_floor
+        self.current_floor = bottom_floor
+
+    def floor_up(self, current_floor):
+        current_floor += 1
+
+    def floor_down(self,current_floor):
+        current_floor -= 1
+    def go_to_floor(self, floor):
+        while floor != self.current_floor:
+            if floor > self.top_floor:
+                print("This floor doesn't exist.")
+                break
+            if floor < self.bottom_floor:
+                print("This floor doesn't exist.")
+                break
+            else:
+                if floor == self.current_floor:
+                    print("You are already in ", self.current_floor, "floor.")
+                if floor > self.current_floor:
+                    self.floor_up(floor)
+                    self.current_floor = floor
+                    print("Floor is currently", self.current_floor)
+                if floor < self.current_floor:
+                    self.floor_down(floor)
+                    self.current_floor = floor
+                    print("Floor is currently", self.current_floor)
 
 
+elevator = Elevator(1, 9)
 
-    def floor_up:
-
-
-    def floor_down:
-
+elevator.floor_up(1)
---
+++ scrap.py
@@ -0,0 +1,15 @@
+
+
+https://www.rovio.com/articles/angry-birds-friends-teams-up-with-the-world-health-organization-for-a-healthier-future/
+
+https://www.rovio.com/articles/the-future-of-remote-work-at-rovio/
+
+https://www.who.int/news/item/26-07-2021-the-who-and-angry-birds-friends-encourage-communities-to-stay-active-during-covid-19
+
+Osavuosikatsaus 2022
+https://investors.rovio.com/sites/rovio-ir-v2/files/2022-08/Rovio%20Q1-2022_osavuosikatsaus_0.pdf
+
+
+https://techxplore.com/news/2020-08-pandemic-angry-birds-maker-profits.html
+
+https://www.playfinland.fi/covid-19
